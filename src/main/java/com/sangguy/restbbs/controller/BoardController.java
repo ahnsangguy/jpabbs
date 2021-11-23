5338e5d29d50d26f92cb6ec38cd7db189410321a
name <ahnsangguy@naver.com>
2021-11-23 23:17:24 +0900

    게시판 글 작성 및 수정 기능 추가, Javax Validation 사용하여 글 작성 시 유효성 체크

--- src/main/java/com/sangguy/restbbs/controller/BoardController.java
+++ src/main/java/com/sangguy/restbbs/controller/BoardController.java
@@ -1,16 +1,58 @@
 package com.sangguy.restbbs.controller;
 
+import com.sangguy.restbbs.model.Board;
+import com.sangguy.restbbs.repository.BoardRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
 
 @Controller
 @RequestMapping("/board")
 public class BoardController {
 
+    @Autowired
+    private BoardRepository boardRepository;
+
     @GetMapping("/list")
-    public String list() {
+    public String list(Model model) {
+
+        List<Board> boards = boardRepository.findAll();
+        model.addAttribute("boards", boards);
 
         return "board/list";
+    }
+
+    @GetMapping("/form")
+    public String getForm(Model model, @RequestParam(required = false) Long id) {
+
+        if (id == null) {
+
+            model.addAttribute("board", new Board());
+
+        } else {
+
+            Board board = boardRepository.findById(id).orElse(new Board());
+            model.addAttribute("board", board);
+        }
+
+        return "board/form";
+    }
+
+    @PostMapping("/form")
+    public String postForm(@Valid Board board, BindingResult bindingResult) {
+
+        if (bindingResult.hasErrors()) {
+
+            return "board/form";
+        }
+
+        boardRepository.save(board);
+
+        return "redirect:/board/list";
     }
 }
---
+++ src/main/java/com/sangguy/restbbs/model/Board.java
@@ -0,0 +1,27 @@
+package com.sangguy.restbbs.model;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
+@Getter
+@Setter
+public class Board {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    @Size(min = 2, max = 100)
+    private String title;
+    private String content;
+}
