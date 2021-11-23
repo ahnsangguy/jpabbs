4bd6bd8692763a5913b1d8a3d68bcb54192149fa
name <ahnsangguy@naver.com>
2021-11-23 20:40:49 +0900

    init

---
+++ src/main/java/com/sangguy/restbbs/RestbbsApplication.java
@@ -0,0 +1,13 @@
+package com.sangguy.restbbs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestbbsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestbbsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sangguy/restbbs/RestbbsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sangguy.restbbs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestbbsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
